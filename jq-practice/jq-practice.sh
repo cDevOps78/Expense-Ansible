919a239a8726a0ddfd9022f2d89f10bbc0991ab9
chaitu1 <chaitu143@gmail.com>
2024-05-05 14:58:06 +0530

    push

--- jq-practice/jq-practice.sh
+++ jq-practice/jq-practice.sh
@@ -1,3 +1,6 @@
-jq '.' file1.json 
-jq '.name1'  file1.json
-jq '.name2.name20' file1.json 
+# jq '.' file1.json 
+# jq '.name1'  file1.json
+# jq '.name2.name20' file1.json 
+
+
+echo "name1: $(jq -r .name1 file1.json)"
