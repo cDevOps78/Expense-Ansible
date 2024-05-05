b54800a6c220c4b07f5b213d7709cde3b00d0dbf
chaitu1 <chaitu143@gmail.com>
2024-05-05 14:59:34 +0530

    push

--- jq-practice/jq-practice.sh
+++ jq-practice/jq-practice.sh
@@ -3,4 +3,4 @@
 # jq '.name2.name20' file1.json 
 
 
-echo -e "name1: $(jq -r .name1 file1.json)\nname3: $(jq -r .name1 file1.json)"
+# echo -e "name1: $(jq -r .name1 file1.json)\nname3: $(jq -r .name3 file1.json)"
